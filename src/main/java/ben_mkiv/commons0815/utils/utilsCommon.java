afba3d586eebe0450deb6ff1178a5887d752003f
ben-mkiv <advoztyerusdowranhivcin2@gmail.com>
2018-07-21 01:12:46 +0200

    fixed experience functions (enderio was reference... not a good one -.-)

--- src/main/java/ben_mkiv/commons0815/utils/utilsCommon.java
+++ src/main/java/ben_mkiv/commons0815/utils/utilsCommon.java
@@ -4,7 +4,6 @@ import com.google.common.base.Charsets;
 import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
@@ -92,27 +91,45 @@ public class utilsCommon {
 		return 0;
 	}
 
-	public static int getExpFromLevel(int level) {
-		if (level > 30) {
-			return (int) (4.5 * level * level - 162.5 * level + 2220);
+
+	public static int getLevelFromExp(long experience) {
+		for (int i = 1; i < xpmap.length; i++) {
+			if (xpmap[i] > experience) {
+				return i;
+			}
 		}
-		if (level > 15) {
-			return (int) (2.5 * level * level - 40.5 * level + 360);
-		}
-		return level * level + 6 * level;
+		return xpmap.length;
 	}
 
-	public static int getLevelFromExp(long exp) {
-		if (exp > 1395) {
-			return (int) Math.round((Math.sqrt(72 * exp - 54215) + 325) / 18);
+	private static final int[] xpmap = new int[21863];
+	static {
+		for (int level = 0, res = 0; level < xpmap.length; level++) {
+			res += getXpBarCap(level);
+			if (res < 0) {
+				res = Integer.MAX_VALUE;
+			}
+			xpmap[level] = res;
 		}
-		if (exp > 315) {
-			return (int) Math.round(Math.sqrt(40 * exp - 7839) / 10 + 8.1);
+	}
+
+	public static int getExpFromLevel(int level) {
+		if (level <= 0)
+			return 0;
+
+		if (level >= 21863)
+			return Integer.MAX_VALUE;
+
+		return xpmap[level-1];
+	}
+
+
+	public static int getXpBarCap(int level){
+		if (level >= 30) {
+			return 112 + (level - 30) * 9;
+		} else if (level >= 15) {
+			return 37 + (level - 15) * 5;
 		}
-		if (exp > 0) {
-			return (int) Math.round(Math.sqrt(exp + 9) - 3);
-		}
-		return 0;
+		return 7 + level * 2;
 	}
 
 	public static ArrayList<Class> getClassHierarchy(Class p) {
