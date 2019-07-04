3a1c57ef4b25522ba31a60bdf44f77fdc121d314
ben-mkiv <advoztyerusdowranhivcin2@gmail.com>
2019-07-04 11:54:25 +0200

    fix for openglasses issue #67

--- src/main/java/ben_mkiv/commons0815/utils/utilsClient.java
+++ src/main/java/ben_mkiv/commons0815/utils/utilsClient.java
@@ -1,15 +1,22 @@
 package ben_mkiv.commons0815.utils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
+import java.util.HashMap;
+
 @SideOnly(Side.CLIENT)
 public class utilsClient {
-    public static Minecraft getMC(){ return Minecraft.getMinecraft(); }
+
+    private static HashMap<String, Long> cooldownTimer = new HashMap<>();
 
     public static FontRenderer fontRenderer(){
-        return utilsClient.getMC().fontRenderer;
+        return Minecraft.getMinecraft().fontRenderer;
     }
 
     public static float defaultFOV = Minecraft.getMinecraft().gameSettings.fovSetting;
@@ -17,4 +24,48 @@ public class utilsClient {
     public static void setFOV(float fov){
         Minecraft.getMinecraft().gameSettings.fovSetting = fov;
     }
+
+    public static Entity getFocusedEntity(){
+        EntityPlayer player = Minecraft.getMinecraft().player;
+        return player.isSneaking() ? player : Minecraft.getMinecraft().objectMouseOver.entityHit;
+    }
+
+    public static TileEntity getFocusedTileEntity() {
+        Minecraft mc = Minecraft.getMinecraft();
+        if (mc.objectMouseOver == null)
+            return null;
+
+        if(!mc.objectMouseOver.typeOfHit.equals(RayTraceResult.Type.BLOCK))
+            return null;
+
+        return mc.getMinecraft().player.world.getTileEntity(mc.getMinecraft().objectMouseOver.getBlockPos());
+    }
+
+    public static Entity getEntityLookingAt(){
+        RayTraceResult objectMouseOver = Minecraft.getMinecraft().player.rayTrace(128, 1);
+        if(objectMouseOver != null && objectMouseOver.typeOfHit == RayTraceResult.Type.ENTITY){
+            return objectMouseOver.entityHit;
+        }
+        return null;
+    }
+
+    public static boolean checkCooldown(String identifier) {
+        return checkCooldown(identifier, 500);
+    }
+
+    public static boolean checkCooldown(String identifier, long cooldownTime){
+        long timenow = net.minecraft.client.Minecraft.getSystemTime();
+
+        long timeout = 0;
+        if(cooldownTimer.get(identifier) != null)
+            timeout = cooldownTimer.get(identifier);
+
+        if(timenow - timeout < cooldownTime)
+            return false;
+
+        cooldownTimer.remove(identifier);
+        cooldownTimer.put(identifier, timenow);
+        return true;
+    }
+
 }
--- src/main/java/ben_mkiv/commons0815/utils/utilsCommon.java
+++ src/main/java/ben_mkiv/commons0815/utils/utilsCommon.java
@@ -4,8 +4,6 @@ import com.google.common.base.Charsets;
 import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.projectile.EntitySnowball;
-import net.minecraft.item.ItemSnowball;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
@@ -15,8 +13,6 @@ import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.EnumSkyBlock;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.FMLCommonHandler;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.vecmath.Vector3f;
 import java.awt.*;
@@ -27,11 +23,9 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 public class utilsCommon {
-	private static HashMap<String, Long> cooldownTimer = new HashMap<>();
-
+	/*
 	public static Entity getEntityByUUID(World world, UUID uuid){
 		for(Entity ent : world.getLoadedEntityList())
 			if(ent.getUniqueID().equals(uuid))
@@ -40,13 +34,14 @@ public class utilsCommon {
 		return null;
 	}
 
+	// if this is required put it in a proxy, as it would break servercode here, leaving it in here for reference
 	public static Entity getEntityByUUID(UUID uuid){
 		if(FMLCommonHandler.instance().getEffectiveSide().equals(Side.SERVER))
 			return FMLCommonHandler.instance().getMinecraftServerInstance().getEntityFromUuid(uuid);
 		else
 			return getEntityByUUID(net.minecraft.client.Minecraft.getMinecraft().world, uuid);
 	}
-
+	*/
 
 	public static ArrayList<BlockPos> getSquareOffsets(int radius){
 		ArrayList<BlockPos> list = new ArrayList();
@@ -59,7 +54,6 @@ public class utilsCommon {
 		return list;
 	}
 
-
 	public static TileEntity getTileEntity(int dimensionId, BlockPos pos){
 		World world  = FMLCommonHandler.instance().getMinecraftServerInstance().getWorld(dimensionId);
 		if(world==null)
@@ -70,31 +64,10 @@ public class utilsCommon {
 	public static Map<BlockPos, Block> getSquareBlocks(World world, BlockPos pos, int radius){
 		Map<BlockPos, Block> list = new HashMap<>();
 
-		for(BlockPos val : utilsCommon.getSquareOffsets(radius))
+		for(BlockPos val : getSquareOffsets(radius))
 			list.put(val, world.getBlockState(new BlockPos(pos.getX() + val.getX(), pos.getY(), pos.getZ() + val.getZ())).getBlock());
 
 		return list;
-	}
-
-	@SideOnly(Side.CLIENT)
-	public static boolean checkCooldown(String identifier) {
-		return checkCooldown(identifier, 500);
-	}
-
-	@SideOnly(Side.CLIENT)
-	public static boolean checkCooldown(String identifier, long cooldownTime){
-		long timenow = net.minecraft.client.Minecraft.getSystemTime();
-
-		long timeout = 0;
-		if(cooldownTimer.get(identifier) != null)
-			timeout = cooldownTimer.get(identifier);
-
-		if(timenow - timeout < cooldownTime)
-			return false;
-
-		cooldownTimer.remove(identifier);
-		cooldownTimer.put(identifier, timenow);
-		return true;
 	}
 
 	public static ArrayList<Class> getClassHierarchy(Class p) {
@@ -135,7 +108,7 @@ public class utilsCommon {
 	}
 
 	public static String StringFromResourceLocation(Class modClazz, ResourceLocation rl){
-		return utilsCommon.list2string(utilsCommon.readResourceLocation(modClazz, rl));
+		return list2string(readResourceLocation(modClazz, rl));
 	}
 
 	public static String list2string(ArrayList<String> list){
@@ -149,14 +122,11 @@ public class utilsCommon {
 	}
 
 	public static int getIntFromColor(float red, float green, float blue, float alpha){
-	    Color col = new Color(red, green, blue, alpha);
-	    int val = col.getRGB();
-	    return val;
+	    return new Color(red, green, blue, alpha).getRGB();
 	}
 
 	public static int getIntFromColor(float color[], float alpha){
-		Color col = new Color(color[0], color[1], color[2], alpha);
-		return col.getRGB();
+		return new Color(color[0], color[1], color[2], alpha).getRGB();
 	}
 
 	public static Vector3f getVector3fFromColor(int color){
@@ -165,7 +135,7 @@ public class utilsCommon {
 	}
 
 	public static int getIntFromColor(float red, float green, float blue){
-		return utilsCommon.getIntFromColor(red, green, blue, 0);
+		return getIntFromColor(red, green, blue, 0);
 	}
 	
 	public static boolean isIntColorVisible(int color){
@@ -173,7 +143,8 @@ public class utilsCommon {
 	}
 
 	public static float deg2rad(float deg){
-		return ((float) Math.PI * deg / 180);
+		//return ((float) Math.PI * deg / 180);
+		return (float) Math.toRadians(deg);
 	}
 	
 	public static boolean inRange(double x, double y, double z, double sx, double sy, double sz, double r){
@@ -181,11 +152,13 @@ public class utilsCommon {
 	}
 	
 	public static boolean inRange(Entity player, double sx, double sy, double sz, double r){
-		return inRange(player.posX, player.posY, player.posZ, sx, sy, sz, r);
+		return player.getPositionVector().distanceTo(new Vec3d(sx, sy, sz)) <= r;
+		//return inRange(player.posX, player.posY, player.posZ, sx, sy, sz, r);
 	}
 
 	public static boolean inRange(Entity player, Vec3d pos, double r){
-		return inRange(player.posX, player.posY, player.posZ, pos.x, pos.y, pos.z, r);
+		return player.getPositionVector().distanceTo(pos) <= r;
+		//return inRange(player.posX, player.posY, player.posZ, pos.x, pos.y, pos.z, r);
 	}
 	
 	public static boolean isLookingAt(RayTraceResult pos, float[] target){
@@ -197,40 +170,12 @@ public class utilsCommon {
 		return true;
 	}
 
-	@SideOnly(Side.CLIENT)
-	public static Entity getEntityLookingAt(){
-		RayTraceResult objectMouseOver = net.minecraft.client.Minecraft.getMinecraft().player.rayTrace(128, 1);
-		if(objectMouseOver != null && objectMouseOver.typeOfHit == RayTraceResult.Type.ENTITY){
-			return objectMouseOver.entityHit;
-		}
-		return null;
-	}
-
-
 	public static RayTraceResult rayTrace(Entity entity, double range, float tickAge){
 		Vec3d vec3d = entity.getPositionEyes(tickAge);
 		Vec3d vec3d1 = entity.getLook(tickAge);
 		Vec3d vec3d2 = vec3d.addVector(vec3d1.x * range, vec3d1.y * range, vec3d1.z * range);
 
-		return entity.world.rayTraceBlocks(vec3d, vec3d2);
-	}
-
-	@SideOnly(Side.CLIENT)
-	public static Entity getFocusedEntity(){
-		EntityPlayer player = net.minecraft.client.Minecraft.getMinecraft().player;
-		return player.isSneaking() ? player : net.minecraft.client.Minecraft.getMinecraft().objectMouseOver.entityHit;
-	}
-
-	@SideOnly(Side.CLIENT)
-	public static TileEntity getFocusedTileEntity() {
-		net.minecraft.client.Minecraft mc = net.minecraft.client.Minecraft.getMinecraft();
-		if (mc.objectMouseOver == null)
-			return null;
-
-		if(!mc.getMinecraft().objectMouseOver.typeOfHit.equals(RayTraceResult.Type.BLOCK))
-			return null;
-
-		return mc.getMinecraft().player.world.getTileEntity(mc.getMinecraft().objectMouseOver.getBlockPos());
+		return entity.getEntityWorld().rayTraceBlocks(vec3d, vec3d2);
 	}
 
 	public static int getLightLevelPlayer(EntityPlayer e){
